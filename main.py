d1f5c88344c43048b5be87b8cadd3f05fa376966
Vitalii Abetkin <abvit89@gmail.com>
2016-06-16 03:51:53 +0300

    go

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from unittest.main import TestProgram, loader
 
+# FIXME
+from case import Case
+
 class MFProgram(TestProgram):
+    # FIXME
     pass
 
 class CoLoader(loader.TestLoader):
@@ -8,31 +12,40 @@ class CoLoader(loader.TestLoader):
     def getTestCaseNames(self, testCaseClass):
         """Return a sorted sequence of method names found within testCaseClass
         """
-        def isTestMethod(attrname, testCaseClass=testCaseClass,
-                         prefix=self.testMethodPrefix):
+
+        def isCoro(attrname, testCaseClass=testCaseClass, prefix='co_'):
             return attrname.startswith(prefix) and \
                 callable(getattr(testCaseClass, attrname))
         testFnNames = list(filter(isTestMethod, dir(testCaseClass)))
-        if self.sortTestMethodsUsing:
-            testFnNames.sort(key=functools.cmp_to_key(self.sortTestMethodsUsing))
-        return testFnNames
+        # if self.sortTestMethodsUsing:
+        #     testFnNames.sort(key=functools.cmp_to_key(self.sortTestMethodsUsing))
+        return sorted(testFnNames)
 
     def suiteClass(self, tests):
-        1
+        return Suite(tests)
     
+    def loadTestsFromTestCase(self, testCaseClass):
+        testCaseNames = self.getTestCaseNames(testCaseClass)
+        if not testCaseNames and hasattr(testCaseClass, 'runTest'):
+            testCaseNames = ['runTest']
+        loaded_suite = self.suiteClass(map(testCaseClass, testCaseNames))
+        return loaded_suite
+
     def loadTestsFromModule(self, module, *args, pattern=None, **kws):
         tests = []
         for name in dir(module):
             obj = getattr(module, name)
-            if isinstance(obj, type) and issubclass(obj, case.TestCase):
+            if isinstance(obj, type) and issubclass(obj, Case):
                 tests.append(self.loadTestsFromTestCase(obj))
 
         return tests
 
 
+
+
 class MFRunner(object):
 
-    resultclass = TextTestResult # ?
+    # resultclass = TextTestResult
 
     def __init__(self, stream=None, descriptions=True, verbosity=1,
                  failfast=False, buffer=False, resultclass=None, warnings=None,
@@ -42,89 +55,30 @@ class MFRunner(object):
         Subclasses should accept **kwargs to ensure compatibility as the
         interface changes.
         """
-        if stream is None:
-            stream = sys.stderr
-        self.stream = _WritelnDecorator(stream)
-        self.descriptions = descriptions
-        self.verbosity = verbosity
-        self.failfast = failfast
-        self.buffer = buffer
-        self.tb_locals = tb_locals
-        self.warnings = warnings
-        if resultclass is not None:
-            self.resultclass = resultclass
+        # if stream is None:
+        #     stream = sys.stderr
+        # self.stream = _WritelnDecorator(stream)
+        # self.descriptions = descriptions
+        # self.verbosity = verbosity
+        # self.failfast = failfast
+        # self.buffer = buffer
+        # self.tb_locals = tb_locals
+        # self.warnings = warnings
+        # if resultclass is not None:
+        #     self.resultclass = resultclass
 
-    def _makeResult(self):
-        return self.resultclass(self.stream, self.descriptions, self.verbosity)
-
-    def run(self, test):
+    def run(self, item):
         "Run the given test case or test suite."
-        result = self._makeResult()
-        registerResult(result)
-        result.failfast = self.failfast
-        result.buffer = self.buffer
-        result.tb_locals = self.tb_locals
-        with warnings.catch_warnings():
-            if self.warnings:
-                # if self.warnings is set, use it to filter all the warnings
-                warnings.simplefilter(self.warnings)
-                # if the filter is 'default' or 'always', special-case the
-                # warnings from the deprecated unittest methods to show them
-                # no more than once per module, because they can be fairly
-                # noisy.  The -Wd and -Wa flags can be used to bypass this
-                # only when self.warnings is None.
-                if self.warnings in ['default', 'always']:
-                    warnings.filterwarnings('module',
-                            category=DeprecationWarning,
-                            message='Please use assert\w+ instead.')
-            startTime = time.time()
-            startTestRun = getattr(result, 'startTestRun', None)
-            if startTestRun is not None:
-                startTestRun()
-            try:
-                test(result)
-            finally:
-                stopTestRun = getattr(result, 'stopTestRun', None)
-                if stopTestRun is not None:
-                    stopTestRun()
-            stopTime = time.time()
-        timeTaken = stopTime - startTime
-        result.printErrors()
-        if hasattr(result, 'separator2'):
-            self.stream.writeln(result.separator2)
-        run = result.testsRun
-        self.stream.writeln("Ran %d test%s in %.3fs" %
-                            (run, run != 1 and "s" or "", timeTaken))
-        self.stream.writeln()
+        item()
 
-        expectedFails = unexpectedSuccesses = skipped = 0
-        try:
-            results = map(len, (result.expectedFailures,
-                                result.unexpectedSuccesses,
-                                result.skipped))
-        except AttributeError:
-            pass
-        else:
-            expectedFails, unexpectedSuccesses, skipped = results
 
-        infos = []
-        if not result.wasSuccessful():
-            self.stream.write("FAILED")
-            failed, errored = len(result.failures), len(result.errors)
-            if failed:
-                infos.append("failures=%d" % failed)
-            if errored:
-                infos.append("errors=%d" % errored)
-        else:
-            self.stream.write("OK")
-        if skipped:
-            infos.append("skipped=%d" % skipped)
-        if expectedFails:
-            infos.append("expected failures=%d" % expectedFails)
-        if unexpectedSuccesses:
-            infos.append("unexpected successes=%d" % unexpectedSuccesses)
-        if infos:
-            self.stream.writeln(" (%s)" % (", ".join(infos),))
-        else:
-            self.stream.write("\n")
-        return result
+class Suite(object):
+    def __init__(self, co_items):
+        self.co_items = co_items
+    
+    def __call__(self):
+        'FIXME'
+        for co in self.co_items:
+            co.send(None)
+        main = klass.main
+        main()
