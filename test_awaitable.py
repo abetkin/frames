d819cada9af48e16f2d7072d466078c7259b45cc
Vitalii Abetkin <abvit89@gmail.com>
2016-08-25 22:28:57 +0300

    _

--- test_awaitable.py
+++ test_awaitable.py
@@ -1,33 +1,47 @@
 
+from contextlib import suppress
+
+class Target:
+
+    def method(self):
+        return 1
+
 
 class A:
 
     def __init__(self, get_co):
-        self.__class__.ins = self
         self.get_co = get_co
 
-    def wrapped(self):
-        co = self.get_co()
-        co.send(100)
+    def wrap(self, _orig=Target.method):
+        def wrapper(ins):
+            ret = _orig(ins)
+            co = self.get_co()
+            with suppress(StopIteration):
+                co.send(ret)
+            return ret
+        setattr(Target, 'method', wrapper)
+        return wrapper
 
     
     def __await__(self):
-        yield self
-        return 1
+        self.wrap()
+        ret = (yield self)
+        return ret
 
 
 
-async def f(get_co):
-    v = await A(get_co)
-    print('v', v)
+async def cofunc(A):
+    v = await A
+    print('spy: result =', v)
+
 
 def run(f):
-    
     def get_co():
         return co
-    co = f(get_co)
+    aa = A(get_co)
+    co = f(aa)
     co.send(None)
 
 if __name__ == '__main__':
-    run(f)
-    A.ins.wrapped()
+    run(cofunc)
+    print('result =', Target().method())
